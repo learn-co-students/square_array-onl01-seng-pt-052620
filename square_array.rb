aad052272791cda270f20c7eff91975780b885da
Uche Nweke Jr <unwekejr@gmail.com>
2020-06-04 23:34:39 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
+
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |number| 
+    number ** 2
+    new_number = number ** 2
+    new_array << new_number
+    
+end
+return new_array
 end
